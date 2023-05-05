3f66ea377774a508de92d9d330688713317ef54e
Nickoff <v3136820@gmail.com>
2023-05-05 16:38:10 +0300

    feat: add errors handler to 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,3 +3,4 @@ const fs = require('fs');
 const path = require('path');
 const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
 readStream.on('data', data => stdout.write(data.toString().trim()));
+readStream.on('error', err => console.log(err));
