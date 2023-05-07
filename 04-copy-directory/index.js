59b010286a211924d33d7b42db6093c9559fd408
Nickoff <v3136820@gmail.com>
2023-05-07 16:17:47 +0300

    feat: add complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+async function copyDir() {
+  const dirPath = path.join(__dirname, '/files');
+  const newDirPath = path.join(`${dirPath}-copy`);
+  const data = await fs.promises.readdir(dirPath);
+  await fs.promises.mkdir(newDirPath, { recursive: true });
+  data.forEach((elem) => {
+    try {
+      (async () => {
+        const fileStat = await fs.promises.stat(path.join(dirPath, elem));
+        if (fileStat.isFile()) {
+          await fs.promises.copyFile(path.join(dirPath, elem), path.join(newDirPath, elem));
+        }
+      })();
+    } catch (err) {
+      console.error(err);
+    }
+  });
+}
+
+try {
+  copyDir();
+} catch (err) {
+  console.error(err);
+}
