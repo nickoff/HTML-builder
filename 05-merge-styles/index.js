a6984564d0ff83e4ef222cc52e0e4b3b40e8f8eb
Nickoff <v3136820@gmail.com>
2023-05-08 02:52:38 +0300

    feat: complete 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,7 +1,23 @@
 const fs = require('fs');
-const readline = require('readline');
 const path = require('path');
-const {
-  stdin: input,
-  stdout: output,
-} = process;
+
+const pathDirCss = path.join(__dirname, './styles');
+const pathDirDist = path.join(__dirname, './project-dist');
+const fileNameBundle = 'bundle.css';
+const cssFileExtname = '.css';
+
+try {
+  (async () => {
+    const arrStyleFiles = await fs.promises.readdir(pathDirCss);
+    const arrCssFiles = arrStyleFiles
+      .filter((elem) => path.extname(path.join(pathDirCss, elem)) === cssFileExtname);
+    const writeStream = fs.createWriteStream(path.join(pathDirDist, fileNameBundle));
+    arrCssFiles.forEach((elem) => {
+      const readStream = fs.createReadStream(path.join(pathDirCss, elem));
+      readStream.on('data', (data) => writeStream.write(data));
+    });
+  }
+  )();
+} catch (err) {
+  console.error(err);
+}
